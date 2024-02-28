fcd01ebca237d2928bd411e3cca4ad03fd0abf8f
utkrisht sharma <utkrisht@utkrishts-MacBook-Air-2.local>
2024-02-28 13:07:57 -0800

    readme done 69

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
+# app.py
+
 from flask import Flask, render_template, request, redirect, url_for
 from wtforms import Form, StringField, validators
 import random
 import string
-from urllib.parse import quote_plus
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -17,38 +18,35 @@ def generate_slug():
     characters = string.ascii_letters + string.digits
     return ''.join(random.choice(characters) for _ in range(6))
 
-@app.route('/', methods=['GET', 'POST'])  # Allow both GET and POST requests
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    form = URLForm(request.form)  # Pass request.form to the form
+    form = URLForm(request.form)
     if request.method == 'POST' and form.validate():
         original_url = form.url.data
-        
-        # Get custom_slug from form data if present, otherwise set it to None
         custom_slug = form.custom_slug.data
 
-        # Check if a custom slug is provided and is unique
         if custom_slug:
             if custom_slug in url_mapping:
                 return render_template('index.html', form=form, error="Custom slug is not unique, please choose another.")
             else:
                 slug = custom_slug
         else:
-            # Generate a random slug
             slug = generate_slug()
             while slug in url_mapping:
                 slug = generate_slug()
 
         url_mapping[slug] = original_url
 
-        return redirect(url_for('success', slug=quote_plus(slug)))  # Redirect to success route with slug parameter
-    # Fetch all slugs in the url_mapping
+        return redirect(url_for('success', slug=slug))
+
     slugs = url_mapping.keys()
-    return render_template('index.html', form=form, slugs=slugs,  url_mapping=url_mapping)
+    return render_template('index.html', form=form, slugs=slugs, url_mapping=url_mapping)
 
 @app.route('/success/<slug>')
 def success(slug):
     original_url = url_mapping.get(slug)
-    return render_template('success.html', original_url=original_url)
+    short_url = request.url_root + slug
+    return render_template('success.html', original_url=original_url, short_url=short_url)
 
 @app.route('/<slug>')
 def redirect_to_url(slug):
